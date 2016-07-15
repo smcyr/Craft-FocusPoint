83b36753922a4bb2ca12543289a19f976aeee758
smarineau-cyr <samuel.marineau.cyr@gmail.com>
2016-07-15 09:48:01 -0400

    Bug fixes to prevent schema update each time and to prevent an error when deleting assets associated with focuspoint fields

--- focuspoint/FocusPointPlugin.php
+++ focuspoint/FocusPointPlugin.php
@@ -5,49 +5,59 @@ namespace Craft;
 class FocusPointPlugin extends BasePlugin
 {
 
-    public function getName()
-    {
-        return Craft::t('Focus Point');
-    }
+	public function getName()
+	{
+		return Craft::t('Focus Point');
+	}
 
-    public function getVersion()
-    {
-        return '1.0.4';
-    }
+	public function getVersion()
+	{
+		return '1.0.5';
+	}
 
-    public function getDeveloper()
-    {
-        return 'Samuel Marineau-Cyr';
-    }
+	public function getSchemaVersion()
+	{
+		return '1.0.1';
+	}
 
-    public function getDeveloperUrl()
-    {
-        return 'https://github.com/smcyr';
-    }
+	public function getDeveloper()
+	{
+		return 'Samuel Marineau-Cyr';
+	}
 
-    public function getPluginUrl()
-    {
-        return 'https://github.com/smcyr/Craft-FocusPoint';
-    }
+	public function getDeveloperUrl()
+	{
+		return 'https://github.com/smcyr';
+	}
 
-    public function getDocumentationUrl()
-    {
-        return $this->getPluginUrl() . '/blob/master/README.md';
-    }
+	public function getPluginUrl()
+	{
+		return 'https://github.com/smcyr/Craft-FocusPoint';
+	}
 
-    public function getReleaseFeedUrl()
-    {
-        return 'https://raw.githubusercontent.com/smcyr/Craft-FocusPoint/master/changelog.json';
-    }
+	public function getDocumentationUrl()
+	{
+		return $this->getPluginUrl() . '/blob/master/README.md';
+	}
 
-    public function init()
-    {
-        craft()->on('elements.onBeforeDeleteElements', function (Event $event) {
-            $elementIds = $event->params['elementIds'];
-            foreach ($elementIds as $elementId) {
-                craft()->focusPoint_focusPoint->deleteFocusPointRecordsBySourceId($elementId);
-            }
-        });
-    }
+	public function getReleaseFeedUrl()
+	{
+		return 'https://raw.githubusercontent.com/smcyr/Craft-FocusPoint/master/changelog.json';
+	}
+
+	public function init()
+	{
+		craft()->on('elements.onBeforeDeleteElements', function (Event $event) {
+			$elementIds = $event->params['elementIds'];
+			foreach ($elementIds as $elementId) {
+				craft()->focusPoint_focusPoint->deleteFocusPointRecordsBySourceId($elementId);
+			}
+		});
+
+		craft()->on('assets.onBeforeDeleteAsset', function (Event $event) {
+			$asset = $event->params["asset"];
+			craft()->focusPoint_focusPoint->deleteFocusPointRecordsByAssetId($asset->id);
+		});
+	}
 
 }
